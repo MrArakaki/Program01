c1508333044e3a73948750bdd751cb3e0a7944d7
Marcelo Ribeiro <arakaki2k2@gmail.com>
2020-01-14 16:20:04 -0300

    Duplo for para matriz

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,31 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int [][] mat =new int [n][n];
+		
+		for (int i=0; i < mat.length; i++) {
+			for (int j=0; j < mat[i].length; j++) {
+				
+			mat [i][j]=sc.nextInt();
+			}
+			
+		}
+		
+		
+		
+		sc.close();
 	}
 
 }
